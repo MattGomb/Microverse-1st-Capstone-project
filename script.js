02228de8009dc29fd201511d97a87a4acd3e57b7
Matyas Gombos <gombosmm@gmail.com>
2022-09-12 11:11:21 -0500

    Add navbar, hamburger menu, lato font

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleHamburger() {
+  var dropDown = document.getElementById("myLinks");
+  if (dropDown.style.display === "block") {
+    dropDown.style.display = "none";
+  } else {
+    dropDown.style.display = "block";
+  }
+}
