39ff61b8623cac8ee8af1cd1c81d896b696c5ec7
Matyas Gombos <gombosmm@gmail.com>
2022-09-12 19:06:00 -0500

    Fix first round of linter errors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleHamburger() {
-  var dropDown = document.getElementById("myLinks");
-  if (dropDown.style.display === "block") {
-    dropDown.style.display = "none";
+  var dropDown = document.getElementById('myLinks')
+  if (dropDown.style.display === 'block') {
+    dropDown.style.display = 'none',
   } else {
-    dropDown.style.display = "block";
+    dropDown.style.display = 'block',
   }
 }
